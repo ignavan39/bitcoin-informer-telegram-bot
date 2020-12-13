bd915f4443398b9484b9cab3ec14fec2d86cf7f9
kitt3911 <ivanignatenko28@gmail.com>
2020-12-13 13:28:26 +0600

    config added

---
+++ config/config.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+
+const bot_token = process.env.BOT_TOKEN
+
+module.exports = {
+    bot_token
+}
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
-
+const {bot_token} = require("./config/config")
 
 app.listen(PORT, () => {
-    console.log(`Server startred on port ${PORT}`);
+    console.log(`Server startred on port ${PORT}`)
 });
