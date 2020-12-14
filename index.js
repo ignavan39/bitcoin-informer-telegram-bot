c250a4984043e88295f37ef6fb44d0256232d0b4
Dmitriy Shelgunov <shelgunovdima3@gmail.com>
2020-12-14 16:15:57 +0600

    merge commit

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+const {Telegraf} = require("telegraf");
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8080;
-const {bot_token} = require("./config/config")
+const Telegram = require('telegraf/telegram')
+require('dotenv').config()
 
-app.listen(PORT, () => {
-    console.log(`Server startred on port ${PORT}`)
-});
+const bot = new Telegraf(process.env["BOT_TOKEN"])
+bot.launch()
+
+const telegram = new Telegram(process.env["MY_TOKEN"])
+telegram.sendMessage('603121011', 'message')
+
+
+
+
