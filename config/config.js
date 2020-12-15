2c29cb904096d35f23cd0b1d141ff4e0b677bd00
Dmitriy Shelgunov <shelgunovdima3@gmail.com>
2020-12-15 16:45:45 +0600

    merge commit

--- config/config.js
+++ config/config.js
@@ -4,7 +4,6 @@ const { Telegraf } = require('telegraf')
 const bot_token = process.env.BOT_TOKEN
 const bot = new Telegraf(bot_token)
 
-
 module.exports = {
     bot
 }
--- index.js
+++ index.js
@@ -3,8 +3,28 @@ const {bot} = require("./config/config")
 const app = express();
 
 
-//const Telegram = require('telegraf/telegram')
-//bot.launch()
+bot.hears(/(\w+) vs (\w+) to -(\d+)/, ctx => {
+    console.log(ctx.match)
+})
 
-//const telegram = new Telegram(process.env["MY_TOKEN"])
-//telegram.sendMessage('603121011', 'message')
+const Telegram = require('telegraf/telegram')
+
+const telegram = new Telegram(process.env["BOT_TOKEN"])
+
+/*
+const changeTitleByExchange = (chatId, title) => {
+    telegram.setChatTitle(chatId, title)
+}
+
+
+
+
+
+telegram.setChatTitle('-455245562','1234212')
+telegram.sendMessage('-455245562', 'message')
+telegram.getChat('-455245562').then(
+    ctx => console.log(ctx)
+)
+telegram.getUpdates().then(ctx => console.log(ctx))
+
+ */
